ef24f095743d91262aa17c1a0e9096ed9370ef7a
csae8092 <peter.andorfer@oeaw.ac.at>
2022-12-05 22:08:59 +0100

    download only one document

--- scripts/fetch_mets.py
+++ scripts/fetch_mets.py
@@ -1,16 +1,20 @@
 import os
 from transkribus_utils.transkribus_utils import ACDHTranskribusUtils
 
-user = os.environ.get('TR_USER')
-pw = os.environ.get('TR_PW')
-#col_id = os.environ.get('COL_ID')
-doc_id = os.environ.get('DOC_ID')
+user = os.environ.get("TR_USER")
+pw = os.environ.get("TR_PW")
+col_id = os.environ.get("COL_ID")
+doc_id = os.environ.get("DOC_ID")
 
 transkribus_client = ACDHTranskribusUtils(
-    user=user,
-    password=pw,
-    transkribus_base_url="https://transkribus.eu/TrpServer/rest"
+    user=user, password=pw, transkribus_base_url="https://transkribus.eu/TrpServer/rest"
 )
 
-#mpr_docs = transkribus_client.collection_to_mets(col_id, file_path='./mets')
-mpr_docs = transkribus_client.document_to_mets(doc_id, file_path='./mets')
+# mpr_docs = transkribus_client.collection_to_mets(col_id, file_path='./mets')
+mpr_docs = transkribus_client.document_to_mets(
+    doc_id,
+    file_path="./mets",
+    filter_by_doc_ids=[
+        doc_id,
+    ],
+)
